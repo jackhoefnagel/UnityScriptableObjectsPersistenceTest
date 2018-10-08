2f8c94ca6400f01d4f174cb5067bcfdfc1b8374d
Jack Hoefnagel <jackhoefnagel@gmail.com>
2018-10-08 12:11:56 +0200

    Test project to show that code-added values get deleted after recompile

---
+++ ScriptableObjects Persistence Test/Assets/Scripts/ExampleMonobehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleMonobehaviour : MonoBehaviour {
+
+    public ExampleSO exampleSO;
+
+    public void DoChangeListInScriptableObject(){
+        for (int i = 0; i < 5; i++)
+        {
+            ExampleStruct newExampleStruct = new ExampleStruct("code-added value", (float)i);
+            exampleSO.exampleStructs.Add(newExampleStruct);
+        }
+    }
+
+}
+
+[System.Serializable]
+public class ExampleStruct
+{
+    public string exampleString;
+    public float exampleFloat;
+
+    public ExampleStruct(string newString, float newFloat)
+    {
+        exampleString = newString;
+        exampleFloat = newFloat;
+    }
+}
---
+++ ScriptableObjects Persistence Test/Assets/Scripts/ScriptableObjectScripts/ExampleSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Test SO")]
+public class ExampleSO : ScriptableObject {
+   public List<ExampleStruct> exampleStructs;
+    
+}
